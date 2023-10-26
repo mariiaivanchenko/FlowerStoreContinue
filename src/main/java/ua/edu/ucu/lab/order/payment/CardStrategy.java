120e2672ef1a74245dacc62027f1584b9b9ff507
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2023-10-26 23:01:54 +0300

    Update CardStrategy.java

--- src/main/java/ua/edu/ucu/lab/order/payment/CardStrategy.java
+++ src/main/java/ua/edu/ucu/lab/order/payment/CardStrategy.java
@@ -1,13 +1,9 @@
 package ua.edu.ucu.lab.order.payment;
 
 public class CardStrategy implements PaymentStrategy {
-    private static final double cardBalance = 1000;
+    private static final double CardBalance = 1000;
     @Override
     public boolean pay(double price) {
-        if (price <= cardBalance){
-            return true;
-        } else{
-            return false;
-        }
+        return price <= CardBalance;
     }
 }
