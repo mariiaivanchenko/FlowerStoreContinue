dc9b3c4d0e4a183b60050530392b48af9f3a1b48
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2023-10-26 22:52:16 +0300

    Update FlowerController.java

--- src/main/java/ua/edu/ucu/lab/flower/FlowerController.java
+++ src/main/java/ua/edu/ucu/lab/flower/FlowerController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/flower")
 public class FlowerController {
-    private static final double sepal = 10;
-    private static final double prce = 20;
+    private static final int sepal = 10;
+    private static final int prce = 20;
     @GetMapping("/")
     public Flower getFlower() {
 	return new Flower(sepal, prce, FlowerColor.BLUE, FlowerType.ROSE);
