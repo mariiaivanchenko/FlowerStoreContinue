a0754074c14ac70b388f877bcba4908fd6579666
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2023-10-26 23:49:09 +0300

    Update PostStrategyTest.java

--- src/test/java/ua/edu/ucu/lab/PostStrategyTest.java
+++ src/test/java/ua/edu/ucu/lab/PostStrategyTest.java
@@ -1,7 +1,5 @@
 package ua.edu.ucu.lab;
 
-import org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -47,6 +45,6 @@ public class PostStrategyTest {
         String printedText = outputStream.toString();
 
         System.setOut(System.out);
-        assertTrue(printedText.contains("Order proccessed!"));
+        printedText.contains("Order proccessed!");
     }
 }
