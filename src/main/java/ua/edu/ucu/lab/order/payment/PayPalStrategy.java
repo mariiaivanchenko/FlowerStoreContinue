efd3a0ec512b29841aefbd80444b7c973a3886e0
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2023-10-26 23:01:29 +0300

    Update PayPalStrategy.java

--- src/main/java/ua/edu/ucu/lab/order/payment/PayPalStrategy.java
+++ src/main/java/ua/edu/ucu/lab/order/payment/PayPalStrategy.java
@@ -1,13 +1,9 @@
 package ua.edu.ucu.lab.order.payment;
 
 public class PayPalStrategy implements PaymentStrategy {
-    private static final double walletBalance = 1000;
+    private static final double WalletBalance = 1000;
     @Override
     public boolean pay(double price) {
-        if (price <= walletBalance){
-            return true;
-        } else{
-            return false;
-        }
+        return price <= WalletBalance;
     }
 }
